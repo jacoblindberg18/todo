14b60e5500bce951419482b9d189eb833b2b4d77
jacoblindberg18 <jacob.lindberg18@gmail.com>
2020-02-18 19:30:30 +0100

    finished functionality. added so that you can add and remove elements, cross them out and so that they are stored in local storage

--- todo.js
+++ todo.js
@@ -2,19 +2,33 @@ const todoItems = document.getElementById("todo-items");
 const todoForm = document.getElementById("todo-form");
 const todoItem = document.getElementById("todo-list-item");
 
-function addListItem() {
+if ("todoitems" in localStorage) {
+    todoItems.innerHTML += localStorage.getItem("todoitems");
+}
+
+function addCheckbox(li) {
+    let checkbox = document.createElement("input");
+
+    checkbox.type = "checkbox";
+    checkbox.name = "done";
+    checkbox.setAttribute("id", "checkbox");
+    li.prepend(checkbox);
+}
+
+function createListItem() {
     let item = $("#todo-list-item").val();
 
     let li = document.createElement("li");
     let a = document.createElement("a");
-    // a.href = "#";
-    li.textContent = todoItem.value;
+    addCheckbox(li);
+    li.innerHTML += `<span>${item}</span>`;
+
     todoItems.prepend(li);
     li.classList.add("list-item");
     li.appendChild(a);
-    a.classList.add("remove");
     a.textContent = "x";
-    console.log("list item:", item);
+    a.classList.add("remove");
+    localStorage.setItem("todoitems", todoItems.innerHTML);
 }
 
 function handleOnSubmit(event) {
@@ -22,7 +36,35 @@ function handleOnSubmit(event) {
     if (todoItem.value === "") {
         return;
     }
-    addListItem();
+    createListItem();
+    document.getElementById("todo-form").reset();
 }
 
 todoForm.onsubmit = handleOnSubmit;
+
+todoItems.addEventListener("change", event => {
+    let checkbox = event.target;
+    let parent = checkbox.parentElement;
+    let sibling = document.getElementById("checkbox").nextSibling;
+
+    if (checkbox.checked === true) {
+        document.getElementById("checkbox").setAttribute("checked", 0);
+        sibling.classList.add("completed");
+        localStorage.setItem("todoitems", todoItems.innerHTML);
+    } else if (checkbox.checked === false) {
+        document.getElementById("checkbox").removeAttribute("checked", 0);
+        sibling.classList.remove("completed");
+        localStorage.setItem("todoitems", todoItems.innerHTML);
+    }
+});
+
+todoItems.addEventListener("click", event => {
+    let target = event.target;
+    let hasRemoveClass = target.className;
+
+    if (hasRemoveClass === "remove") {
+        let li = target.parentElement;
+        todoItems.removeChild(li);
+        localStorage.setItem("todoitems", todoItems.innerHTML);
+    }
+});
